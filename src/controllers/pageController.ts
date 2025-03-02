bfcb5604b4258eb6e246e51922afb46168055ccd
vinimax <vini.ciusang@gmail.com>
2025-03-02 09:12:43 -0300

    Banner configuration and page title

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,37 @@
 import { Request, Response } from 'express'; // Importação do express module
 
 export const home = (req: Request, res: Response) => { // Função que renderiza a página inicial
-    res.render('pages/page'); // Renderiza a página home.mustache
+    res.render('pages/page', { // Renderiza a página home.mustache
+        banner:{
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    }); 
 }
 
 export const dogs = (req: Request, res: Response) => { // Função que renderiza a página de cachorros
-    //res.render('pages/dogs'); // Renderiza a página dogs.mustache
+    res.render('pages/page', { // Renderiza a página dogs.mustache
+        banner:{
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => { // Função que renderiza a página de gatos
-    //res.render('pages/cats'); // Renderiza a página cats.mustache
+    res.render('pages/page',{ // Renderiza a página cats.mustache
+        banner:{
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    }); 
 } 
 
 export const fishes = (req: Request, res: Response) => { // Função que renderiza a página de peixes
-    //res.render('pages/fishes'); // Renderiza a página fishes.mustache
+    res.render('pages/page', { // Renderiza a página fishes.mustache
+        banner:{
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    }); 
 }
