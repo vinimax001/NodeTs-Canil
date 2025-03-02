a2a7bfacd5dbc10302e8b80ec782189f8d5c0186
turingadvance <turingadvance@gmail.com>
2025-03-01 21:40:17 -0300

    created html, styled and added images

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'; // Importando o express
+import dotenv from 'dotenv'; // Importando o dotenv
+import mustache from 'mustache-express';   // Importando o mustache
+import path from 'path'; // Importando o path
+
+dotenv.config(); // Carregando as variáveis de ambiente
+
+const server = express(); // Inicializando o servidor
+server.set('view engine', 'mustache'); // Definindo o motor de visualização
+server.set('views', path.join(__dirname, 'views')); // Definindo o diretório de views
+server.engine('mustache', mustache()); // Definindo o motor de visualização
+
+server.use(express.static(path.join(__dirname, '../public'))); // Definindo o diretório de arquivos estáticos
+
+// Rota principal
+
+
+server.listen(process.env.PORT); // Inicializando o servidor na porta definida no arquivo .env
